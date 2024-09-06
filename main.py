85ea5fe468320276834ce02fe0d33ad18a2d53f5
iomegak12 <jd.ramkumar@gmail.com>
2024-09-06 18:22:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import uvicorn
 from art import tprint
 
 if __name__ == "__main__":
-    tprint("Stores and Items", font="random")
-
-    uvicorn.run("store_service.api:app", port=8000, reload=True)
+    tprint("Welcome")
